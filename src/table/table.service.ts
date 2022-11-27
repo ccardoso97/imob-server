d6a7e53cf10f1d1073111b4d723e7e9e29c5a36b
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-11-27 20:23:31 -0300

    dto

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { CreateTableDto } from "./dto/create.table.dto";
+import { Table } from "./entities/table.entity";
 
 @Injectable()
 export class TableService{
+    tables: Table[] = [];
+
     findAll() {
-        return 'Buscar todos os imóveis'
+        return this.tables;
     }
     create(createTableDto: CreateTableDto) {
-        return 'Adicionar um imóvel: ' + JSON.stringify(createTableDto)
+        const table: Table = { id: 'random_id', ...createTableDto}
+        this.tables.push(table)
+        return table;
     }
 }
