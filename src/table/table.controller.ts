93e6b5bb78fed71c71b7a8085ef9ee5794c479b0
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-11-28 19:38:17 -0300

    Api Operation

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTableDto } from './dto/create.table.dto';
 import { TableService } from './table.service';
-import { ApiTags } from '@nestjs/swagger/dist/decorators';
+import { ApiOperation, ApiTags } from '@nestjs/swagger/dist/decorators';
+import { Table } from './entities/table.entity';
 
 @ApiTags('table')
 @Controller('table')
@@ -9,12 +10,26 @@ export class TableController {
   constructor(private readonly tableService: TableService) {}
 
   @Get()
-  findAll() {
+  @ApiOperation({
+    summary: 'Mostrar todos os imóveis'
+  })
+  findAll(): Promise<Table[]> {
     return this.tableService.findAll();
   }
 
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Mostrar um imóvel'
+  })
+  findOne(@Param('id') id: string): Promise<Table> {
+    return this.tableService.findOne(id);
+  }
+
   @Post()
-  create(@Body() dto: CreateTableDto) {
+  @ApiOperation({
+    summary: 'Adicionar imóveis'
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
   }
 }
