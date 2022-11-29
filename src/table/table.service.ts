7620cd0112e8587f7b4987afa7a749ef5457b526
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-11-28 21:39:03 -0300

    tratamento de erro

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { ApiUnprocessableEntityResponse } from '@nestjs/swagger';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './dto/create.table.dto';
 import { UpdateTableDto } from './dto/update.table.dto';
@@ -12,31 +17,53 @@ export class TableService {
     return this.prisma.table.findMany();
   }
 
-  findOne(id: string): Promise<Table> {
-    return this.prisma.table.findUnique({
+  async findById(id: string): Promise<Table> {
+    const record = await this.prisma.table.findUnique({
       where: {
         id,
       },
     });
+
+    if (!record) {
+      throw new NotFoundException(`ID '${id}' não encontrado.`);
+    }
+
+    return record;
+  }
+
+  async findOne(id: string): Promise<Table> {
+    return this.findById(id);
   }
 
   create(dto: CreateTableDto): Promise<Table> {
     const data: Table = { ...dto };
-    return this.prisma.table.create({ data });
+    return this.prisma.table.create({ data }).catch(this.handleError);
   }
 
-  update(id: string, dto: UpdateTableDto): Promise<Table> {
+  async update(id: string, dto: UpdateTableDto): Promise<Table> {
+    await this.findById(id);
+
     const data: Partial<Table> = { ...dto };
 
-    return this.prisma.table.update({
-      where: { id },
-      data,
-    });
+    return this.prisma.table
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
   }
 
   async delete(id: string) {
+    await this.findById(id);
+
     await this.prisma.table.delete({
-     where: { id },
-    })
+      where: { id },
+    });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(lastErrorLine || 'Ocorreu um erro');
   }
 }
